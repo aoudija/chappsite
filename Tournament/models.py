365a205d19998ec692773711403e29ba78eba407
Amine Oudija <aoudija@e3r5p1.1337.ma>
2024-07-17 14:49:38 +0100

    update tournament models

--- Tournament/models.py
+++ Tournament/models.py
@@ -2,23 +2,33 @@ from django.db import models
 from django.contrib.auth.models import User
 from game.models import gameCoordinates
 
-
-class Round(models.Model):#number of matches = number of participants - 1
-    pass
-
-class Match(models.Model):#will take two participants
-    participant1 = models.ForeignKey(User)
-    participant2 = models.ForeignKey(User)
-    game: gameCoordinates = models.ForeignKey(gameCoordinates)
-    typeOfMatch = models.CharField(max_length=2,default='rg')
     
 # Create your models here.
-class tournament(models.Model):
+class Tournament(models.Model):
     participant = models.ManyToManyField(User)
     numberParticipants = models.IntegerField(default=4)
+    round = models.IntegerField(default=1)
+    
+    def next_round(self):
+        matches = Match.objects.filter(finished=False, )
+        winners = [match.winner for match in matches]
+        for i in range(0, len(winners), 2):
+            if i + 1 < len(winners):
+                new_match = Match.objects.create(player=winners[i], opponent=winners[i+1],
+                                     typeOfMatch='tr', round=self.round+1)
+                new_match.save()
+        self.round += 1
 
-class base(models.model):
-    pass
+    def is_complete(self):
+        return self.round == self.numberParticipants - 1
 
-class x(base):
-    pass
+class Match(gameCoordinates):#will take two participants
+    player = models.ForeignKey(User)
+    opponent = models.ForeignKey(User)
+    typeOfMatch = models.CharField(max_length=2,default='rg')
+    winner = models.ForeignKey(User)
+    round = models.IntegerField(default=1)
+    scorePlayer = models.IntegerField(default=0)
+    scoreOpponent = models.IntegerField(default=0)
+    finished = models.BooleanField(default=False)
+    tournament = models.ForeignKey(Tournament, )
--- game/models.py
+++ game/models.py
@@ -3,18 +3,12 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class gameCoordinates(models.Model):
-    player = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    opponent = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     px = models.IntegerField(default=5)
     py = models.IntegerField(default=225)
     ox = models.IntegerField(default=730)
     oy = models.IntegerField(default=225)
     # bx = models.IntegerField(default=0)
     # by = models.IntegerField(default=0)
-    # scorePlayer = models.IntegerField(default=0)
-    # scoreOpponent = models.IntegerField(default=0)
-    # finished = models.BooleanField(default=False)
 
-    
-    def __str__(self):
-        return f"{self.player.username} vs {self.opponent.username}"
+    class Meta:
+        abstract = True
